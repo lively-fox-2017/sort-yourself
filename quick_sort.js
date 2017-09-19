5b7a914cb2a842e6f35120ae233911b2cca6398f
Saptanto Sindu <saptanto.sindu@gmail.com>
2017-09-19 21:41:55 +0700

    Selection and Bubble

--- quick_sort.js
+++ quick_sort.js
@@ -1,40 +1,7 @@
 'use strict'
 
-function quickSortPartition(arr, start, end) {
-  let pivot = arr[start];
-
-  let left = start + 1;
-  let right = end;
-  let done = false;
-  while (!done) {
-    while (left <= right && arr[left] <= pivot) {
-      left += 1;
-    }
-    while (arr[right] >= pivot && right >= left) {
-      right -= 1;
-    }
-    if (right < left)
-      done = true;
-    else {
-      let temp = arr[left];
-      arr[left] = arr[right];
-      arr[right] = temp;
-    }
-
-    return right;
-  }
-}
-
-function quickSortCore(arr, start, end) {
-  if (start < end) {
-    let splitPoint = quickSortPartition(arr, start, end);
-    quickSortCore(arr, start, splitPoint - 1);
-    quickSortCore(arr, splitPoint + 1, end);
-  }
-}
-
 function quickSort(arr) {
-  quickSortCore(arr, 0, arr.length - 1);
+
 }
 
 // Driver code
