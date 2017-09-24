3ca1a4841f896585b46530d5c7ed1eadaed36650
nach9 <nur.achdiansyah@gmail.com>
2017-09-24 16:10:33 +0700

    done release 1 , quick sort fail on memory

--- quick_sort.js
+++ quick_sort.js
@@ -1,8 +1,34 @@
 'use strict'
 
-function quickSort(arr) {
+function quickSort(arr,lo,hi) {
+  if(lo<hi){
+    let pi = partition(arr,lo,hi);
 
+    quickSort(arr,lo,pi-1);
+    quickSort(arr,pi+1,hi);
+  }
+  return arr;
 }
+
+function partition(arr,lo,hi){
+  let pivot = arr[hi];
+  let i =lo-1;
+  for(let j=lo;j<=hi-1;j++){
+    if(arr[j]<=pivot){
+      i++;
+      let temp = arr[i];
+      arr[i]=arr[j];
+      arr[j]=temp;
+    }
+  }
+  let temp = arr[i+1];
+  arr[i+1]=arr[hi];
+  arr[hi] = temp;
+  return (i+1);
+}
+
+var test = [3,5,2,7,5,1,12,11,10,99,33,1];
+console.log(quickSort(test,0,test.length-1));
 
 // Driver code
 function main() {
@@ -26,9 +52,9 @@ function sort_from_file(filename) {
 
     console.log(filename)
     console.log("--------")
-    console.log(quickSort(items))
+    console.log(quickSort(items,0,items.length-1))
 
   })
 }
 
-main()
+// main()
